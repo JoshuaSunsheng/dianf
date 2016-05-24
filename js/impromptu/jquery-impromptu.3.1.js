4d97eb30f1b4649c181bedec1eefd9c06fb66bb1
leosunsheng <su371128017@qq.com>
2016-05-24 10:55:18 +0800

    Initial commit

---
+++ js/impromptu/jquery-impromptu.3.1.js
@@ -0,0 +1,358 @@
+﻿/*
+ * jQuery Impromptu
+ * By: Trent Richardson [http://trentrichardson.com]
+ * Version 3.1
+ * Last Modified: 3/30/2010
+ * 
+ * Copyright 2010 Trent Richardson
+ * Dual licensed under the MIT and GPL licenses.
+ * http://trentrichardson.com/Impromptu/GPL-LICENSE.txt
+ * http://trentrichardson.com/Impromptu/MIT-LICENSE.txt
+ * 
+ */
+ 
+(function(jQuery) {
+	jQuery.prompt = function(message, options) {
+		options = jQuery.extend({},jQuery.prompt.defaults,options);
+		jQuery.prompt.currentPrefix = options.prefix;
+
+		var ie6		= (jQuery.browser.msie && jQuery.browser.version < 7);
+		var $body	= jQuery(document.body);
+		var $window	= jQuery(window);
+		
+		options.classes = jQuery.trim(options.classes);
+		if(options.classes != '')
+			options.classes = ' '+ options.classes;
+			
+		//build the box and fade
+		var msgbox = '<div class="'+ options.prefix +'box'+ options.classes +'" id="'+ options.prefix +'box">';
+		if(options.useiframe && ((jQuery('object, applet').length > 0) || ie6)) {
+			msgbox += '<iframe src="javascript:false;" style="display:block;position:absolute;z-index:-1;" class="'+ options.prefix +'fade" id="'+ options.prefix +'fade"></iframe>';
+		} else {
+			if(ie6) {
+				jQuery('select').css('visibility','hidden');
+			}
+			msgbox +='<div class="'+ options.prefix +'fade" id="'+ options.prefix +'fade"></div>';
+		}
+		msgbox += '<div class="'+ options.prefix +'" id="'+ options.prefix +'"><div class="'+ options.prefix +'container"><div class="';
+		msgbox += options.prefix +'close">×</div><div id="'+ options.prefix +'states"></div>';
+		msgbox += options.prefix +'close"></div><div id="'+ options.prefix +'states"></div>';
+		msgbox += '</div></div></div>';
+
+		var $jqib	= jQuery(msgbox).appendTo($body);
+		var $jqi	= $jqib.children('#'+ options.prefix);
+		var $jqif	= $jqib.children('#'+ options.prefix +'fade');
+
+		//if a string was passed, convert to a single state
+		if(message.constructor == String){
+			message = {
+				state0: {
+					html: message,
+				 	buttons: options.buttons,
+				 	focus: options.focus,
+				 	submit: options.submit
+			 	}
+		 	};
+		}
+
+		//build the states
+		var states = "";
+
+		jQuery.each(message,function(statename,stateobj){
+			stateobj = jQuery.extend({},jQuery.prompt.defaults.state,stateobj);
+			message[statename] = stateobj;
+
+			states += '<div id="'+ options.prefix +'_state_'+ statename +'" class="'+ options.prefix + '_state" style="display:none;"><div class="'+ options.prefix +'message">' + stateobj.html +'</div><div class="'+ options.prefix +'buttons">';
+			jQuery.each(stateobj.buttons, function(k, v){
+				if(typeof v == 'object')
+					states += '<button name="' + options.prefix + '_' + statename + '_button' + v.title.replace(/[^a-z0-9]+/gi,'') + '" id="' + options.prefix + '_' + statename + '_button' + v.title.replace(/[^a-z0-9]+/gi,'') + '" value="' + v.value + '">' + v.title + '</button>';
+				else states += '<button name="' + options.prefix + '_' + statename + '_button' + k + '" id="' + options.prefix +	'_' + statename + '_button' + k + '" value="' + v + '">' + k + '</button>';
+			});
+			states += '</div></div>';
+		});
+
+		//insert the states...
+		$jqi.find('#'+ options.prefix +'states').html(states).children('.'+ options.prefix +'_state:first').css('display','block');
+		$jqi.find('.'+ options.prefix +'buttons:empty').css('display','none');
+		
+		//Events
+		jQuery.each(message,function(statename,stateobj){
+			var $state = $jqi.find('#'+ options.prefix +'_state_'+ statename);
+
+			$state.children('.'+ options.prefix +'buttons').children('button').click(function(){
+				var msg = $state.children('.'+ options.prefix +'message');
+				var clicked = stateobj.buttons[jQuery(this).text()];
+				if(clicked == undefined){
+					for(var i in stateobj.buttons)
+						if(stateobj.buttons[i].title == jQuery(this).text())
+							clicked = stateobj.buttons[i].value;
+				}
+				
+				if(typeof clicked == 'object')
+					clicked = clicked.value;
+				var forminputs = {};
+
+				//collect all form element values from all states
+				jQuery.each($jqi.find('#'+ options.prefix +'states :input').serializeArray(),function(i,obj){
+					if (forminputs[obj.name] === undefined) {
+						forminputs[obj.name] = obj.value;
+					} else if (typeof forminputs[obj.name] == Array || typeof forminputs[obj.name] == 'object') {
+						forminputs[obj.name].push(obj.value);
+					} else {
+						forminputs[obj.name] = [forminputs[obj.name],obj.value];	
+					} 
+				});
+
+				var close = stateobj.submit(clicked,msg,forminputs);
+				if(close === undefined || close) {
+					removePrompt(true,clicked,msg,forminputs);
+				}
+			});
+			$state.find('.'+ options.prefix +'buttons button:eq('+ stateobj.focus +')').addClass(options.prefix +'defaultbutton');
+
+		});
+
+		var ie6scroll = function(){
+			$jqib.css({ top: $window.scrollTop() });
+		};
+
+		var fadeClicked = function(){
+			if(options.persistent){
+				var i = 0;
+				$jqib.addClass(options.prefix +'warning');
+				var intervalid = setInterval(function(){
+					$jqib.toggleClass(options.prefix +'warning');
+					if(i++ > 1){
+						clearInterval(intervalid);
+						$jqib.removeClass(options.prefix +'warning');
+					}
+				}, 100);
+			}
+			else {
+				removePrompt();
+			}
+		};
+		
+		var keyPressEventHandler = function(e){
+			var key = (window.event) ? event.keyCode : e.keyCode; // MSIE or Firefox?
+			
+			//escape key closes
+			if(key==27) {
+				fadeClicked();	
+			}
+			
+			//constrain tabs
+			if (key == 9){
+				var $inputels = jQuery(':input:enabled:visible',$jqib);
+				var fwd = !e.shiftKey && e.target == $inputels[$inputels.length-1];
+				var back = e.shiftKey && e.target == $inputels[0];
+				if (fwd || back) {
+				setTimeout(function(){ 
+					if (!$inputels)
+						return;
+					var el = $inputels[back===true ? $inputels.length-1 : 0];
+
+					if (el)
+						el.focus();						
+				},10);
+				return false;
+				}
+			}
+		};
+		
+		var positionPrompt = function(){
+			$jqib.css({
+				position: (ie6) ? "absolute" : "fixed",
+				height: $window.height(),
+				width: "100%",
+				top: (ie6)? $window.scrollTop() : 0,
+				left: 0,
+				right: 0,
+				bottom: 0
+			});
+			$jqif.css({
+				position: "absolute",
+				height: $window.height(),
+				width: "100%",
+				top: 0,
+				left: 0,
+				right: 0,
+				bottom: 0
+			});
+			$jqi.css({
+				position: "absolute",
+				top: options.top,
+				left: "50%",
+				marginLeft: (($jqi.outerWidth()/2)*-1)
+			});
+		};
+
+		var stylePrompt = function(){
+			$jqif.css({
+				zIndex: options.zIndex,
+				display: "none",
+				opacity: options.opacity
+			});
+			$jqi.css({
+				zIndex: options.zIndex+1,
+				display: "none"
+			});
+			$jqib.css({
+				zIndex: options.zIndex
+			});
+		};
+
+		var removePrompt = function(callCallback, clicked, msg, formvals){
+			$jqi.remove();
+			//ie6, remove the scroll event
+			if(ie6) {
+				$body.unbind('scroll',ie6scroll);
+			}
+			$window.unbind('resize',positionPrompt);
+			$jqif.fadeOut(options.overlayspeed,function(){
+				$jqif.unbind('click',fadeClicked);
+				$jqif.remove();
+				if(callCallback) {
+					options.callback(clicked,msg,formvals);
+				}
+				$jqib.unbind('keypress',keyPressEventHandler);
+				$jqib.remove();
+				if(ie6 && !options.useiframe) {
+					jQuery('select').css('visibility','visible');
+				}
+			});
+		};
+
+		positionPrompt();
+		stylePrompt();
+		
+		//ie6, add a scroll event to fix position:fixed
+		if(ie6) {
+			$window.scroll(ie6scroll);
+		}
+		$jqif.click(fadeClicked);
+		$window.resize(positionPrompt);
+		$jqib.bind("keydown keypress",keyPressEventHandler);
+		$jqi.find('.'+ options.prefix +'close').click(removePrompt);
+
+		//Show it
+		$jqif.fadeIn(options.overlayspeed);
+		$jqi[options.show](options.promptspeed,options.loaded);
+		$jqi.find('#'+ options.prefix +'states .'+ options.prefix +'_state:first .'+ options.prefix +'defaultbutton').focus();
+		
+		if(options.timeout > 0)
+			setTimeout(jQuery.prompt.close,options.timeout);
+
+		return $jqib;
+	};
+	
+	jQuery.prompt.defaults = {
+		prefix:'jqi',
+		classes: '',
+		buttons: {
+			Ok: true
+		},
+	 	loaded: function(){
+
+	 	},
+	  	submit: function(){
+	  		return true;
+		},
+	 	callback: function(){
+
+	 	},
+		opacity: 0.3,
+	 	zIndex: 999,
+	  	overlayspeed: 'slow',
+	   	promptspeed: 'fast',
+   		show: 'fadeIn',
+	   	focus: 0,
+	   	useiframe: false,
+	 	top: "200px",
+	  	persistent: true,
+	  	timeout: 0,
+	  	state: {
+			html: '',
+		 	buttons: {
+		 		Ok: true
+		 	},
+		  	focus: 0,
+		   	submit: function(){
+		   		return true;
+		   }
+	  	}
+	};
+	
+	jQuery.prompt.currentPrefix = jQuery.prompt.defaults.prefix;
+
+	jQuery.prompt.setDefaults = function(o) {
+		jQuery.prompt.defaults = jQuery.extend({}, jQuery.prompt.defaults, o);
+	};
+	
+	jQuery.prompt.setStateDefaults = function(o) {
+		jQuery.prompt.defaults.state = jQuery.extend({}, jQuery.prompt.defaults.state, o);
+	};
+	
+	jQuery.prompt.getStateContent = function(state) {
+		return jQuery('#'+ jQuery.prompt.currentPrefix +'_state_'+ state);
+	};
+	
+	jQuery.prompt.getCurrentState = function() {
+		return jQuery('.'+ jQuery.prompt.currentPrefix +'_state:visible');
+	};
+	
+	jQuery.prompt.getCurrentStateName = function() {
+		var stateid = jQuery.prompt.getCurrentState().attr('id');
+		
+		return stateid.replace(jQuery.prompt.currentPrefix +'_state_','');
+	};
+	
+	jQuery.prompt.goToState = function(state, callback) {
+		jQuery('.'+ jQuery.prompt.currentPrefix +'_state').slideUp('slow');
+		jQuery('#'+ jQuery.prompt.currentPrefix +'_state_'+ state).slideDown('slow',function(){
+			jQuery(this).find('.'+ jQuery.prompt.currentPrefix +'defaultbutton').focus();
+			if (typeof callback == 'function')
+				callback();
+		});
+	};
+	
+	jQuery.prompt.nextState = function(callback) {
+		var $next = jQuery('.'+ jQuery.prompt.currentPrefix +'_state:visible').next();
+
+		jQuery('.'+ jQuery.prompt.currentPrefix +'_state').slideUp('slow');
+		
+		$next.slideDown('slow',function(){
+			$next.find('.'+ jQuery.prompt.currentPrefix +'defaultbutton').focus();
+			if (typeof callback == 'function')
+				callback();
+		});
+	};
+	
+	jQuery.prompt.prevState = function(callback) {
+		var $next = jQuery('.'+ jQuery.prompt.currentPrefix +'_state:visible').prev();
+
+		jQuery('.'+ jQuery.prompt.currentPrefix +'_state').slideUp('slow');
+		
+		$next.slideDown('slow',function(){
+			$next.find('.'+ jQuery.prompt.currentPrefix +'defaultbutton').focus();
+			if (typeof callback == 'function')
+				callback();
+		});
+	};
+	
+	jQuery.prompt.close = function() {
+		jQuery('#'+ jQuery.prompt.currentPrefix +'box').fadeOut('fast',function(){
+        		jQuery(this).remove();
+		});
+	};
+	
+	jQuery.fn.prompt = function(options){
+		if(options == undefined) 
+			options = {};
+		if(options.withDataAndEvents == undefined)
+			options.withDataAndEvents = false;
+			
+		jQuery.prompt(jQuery(this).clone(options.withDataAndEvents).html(),options);
+	}
+	
+})(jQuery);
---
+++ js/nav.js
@@ -0,0 +1,32 @@
+﻿// JavaScript Document
+$(document).ready(function() {
+  var $liCur = $("#nav ul li.current"),
+  curP = $liCur.position().left,
+  curW = $liCur.outerWidth(true),
+  $slider = $(".nav_current"),
+  $navBox = $("#nav");
+  $targetEle = $("#nav ul li a"),
+  $slider.animate({
+    "left": curP,
+    "width": curW
+  });
+  $targetEle.mouseenter(function() {
+    var $_parent = $(this).parent(),
+    _width = $_parent.outerWidth(true),
+    posL = $_parent.position().left;
+    $slider.stop(true, true).animate({
+      "left": posL,
+      "width": _width
+    },
+    "fast");
+  });
+  $navBox.mouseleave(function(cur, wid) {
+    cur = curP;
+    wid = curW;
+    $slider.stop(true, true).animate({
+      "left": cur,
+      "width": wid
+    },
+    "fast");
+  });
+})
